ba1729fbc9292fb7036e2d0cce32afa523702cbb
Michael C. Russell (MCR_Mini) <60197239+Russell-OEA@users.noreply.github.com>
2021-12-16 07:54:42 -0500

    data clean, import set

--- Poverty Measures Calculations.R
+++ Poverty Measures Calculations.R
@@ -1,20 +1,23 @@
-setwd("~/R/Projects/Poverty Measures")
+# Set to working director 
+# setwd("~/R/Projects/Poverty Measures") # Work drive
+ setwd("~/Documents/GitHub/PovertyMeasures") # Home drive
 
 ### Calculates lists of communities meeting different affordable thresholds. 
 ### Project for water group
 ### 12/13/2021
 
 # Load libraries
-#install.packages("tidyverse")
+install.packages("tidyverse")
 library(tidyverse)
+library(readr)
 library(readxl)
 library(ggplot2)
-library(hrbrthemes)
-library(GET)
+# library(hrbrthemes)
+# library(GET)
 
 
 # Create column names for MRI dataset
-MRI_columns <- c("Muni_Code", 
+MRI_columns <- c("CODE", 
                  "Muni",
                  "County",
                  "Region",
@@ -56,33 +59,45 @@ MRI_columns <- c("Muni_Code",
                  
 
 
-### Load datasets
+### Import data
 
-# Use this to load a local copy of MRI data from the working directory:
+# MRI:
+# Source: https://www.nj.gov/dca/home/MuniRevitIndex.html
+MRI_data <- read_excel("2020_MRI_Scores_and_Rankings.xlsx", # imports file
+                       sheet = "2020 MRI - Alphabetical", # selects correct Excel worksheet
+                       col_types = c("text", "text", "text", # sets value columns to numeric, ignore errors
+                                     "text", "text", "text", "text", "text", 
+                                     "text", "numeric", "text", "text", 
+                                     "numeric", "text", "text", "numeric", 
+                                     "text", "text", "numeric", "text", 
+                                     "text", "numeric", "text", "text", 
+                                     "numeric", "text", "text", "numeric", 
+                                     "text", "text", "numeric", "text", 
+                                     "text", "numeric", "text", "text", 
+                                     "numeric", "text"),
+                       col_names = MRI_columns) # names columns 
 
-# not working: download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx",
-# not working:    destfile="./R/Projects/Poverty Measures/2020_MRI_Scores_and_Rankings2.xlsx")
+# LMI
+# Source: https://www.hudexchange.info/programs/acs-low-mod-summary-data/acs-low-mod-summary-data-local-government/
+LMI_data <- LMI_data %>% read_csv("ACS_2015_lowmod_localgov_all.csv", 
+                                         col_types = cols(LOWMOD_PCT = col_number()))
+# County Unemployment Rates
+# Source: https://nj.gov/labor/lpa/LMI_index.html?_ga=2.184612201.1711518215.1639619183-285857380.1639619183
+# CSV available at: 
+NJ_County_Unemp <- read_csv("NJ_County_Unemp.csv")
 
-MRI_data <- read_excel("2020_MRI_Scores_and_Rankings.xlsx", sheet = "2020 MRI - Alphabetical", col_names = MRI_columns)
+# NJ State Municipal Code - FIPS Code Crosswalk
+# Source:
+# CSV available at: 
+Muni_Code_Crosswalk <- read_csv("Muni_Code_Crosswalk.csv")
 
-# Use this to load the 2020 version directly from DCA's website:
-MRI_data <- read_excel("https://www.nj.gov/dca/home/2020_MRI_Scores_and_Rankings.xlsx", sheet = "2020 MRI - Alphabetical", col_names = MRI_columns)
+# NJ Statewide MHI
+# Source: https://www.census.gov/quickfacts/NJ
+NJ_MHI <- 82545
 
-# not working:  GET(url = "https://www.nj.gov/dca/home/2020_MRI_Scores_and_Rankings.xlsx",
-    write_disk(test1) )
-
-# Clean datasets
-MRI_data2 <- MRI_data %>% filter(!is.na(Muni_Code)) #Removes extra columns from Excel sheet
-
-## Sandboxing visualization
-## Not used at this point
-# 
-# Test <- read_excel("Test.xlsx")
-# 
-# p <- ggplot(data = Test, mapping = aes(x = Year, y = Value, color = Type)) +
-#   geom_point(size=3, aes(group=Type)) +
-#   xlab("") +
-#   theme_ipsum() +
-#   facet_wrap(~ Group, ncol = 3) 
-# p
+# Clean data
+MRI_data <- MRI_data %>% filter(!is.na(CODE)) # Removes extra header rows, layout rows, etc. from Excel sheet
+LMI_data <- LMI_data %>% filter(STATE == 34) # Filters out all non-NJ states
+Muni_Code_Crosswalk <- Muni_Code_Crosswalk %>% filter(!is.na(CODE)) # Removes Counties and CDPs
+Muni_Code_Crosswalk <- rename(Muni_Code_Crosswalk, COUSUB = MCD) # Rename header for MCD to align with LMI_data
 
